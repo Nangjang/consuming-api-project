aae2a2fe9aae18d59399a779a6967c1800ce11d3
Nangjang <nangjang.official@gmail.com>
2026-07-25 11:53:20 +0900

    add result page,etc.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -9,6 +9,13 @@ def compare_teams():
     response = requests.get('http://127.0.0.1:5000/')
     return render_template('index.html', teams=response.json()['Teams'])
 
+@app.route('/teamvsteam', methods=['GET'])
+def teamvsteam():
+    team1 = request.args.get('team1')
+    team2 = request.args.get('team2')
+
+    response = requests.get('http://127.0.0.1:5000/api/teamvsteam?team1={}&team2={}'.format(team1, team2))
+    return render_template('index.html', teamvsteam=response.json())
 
 if __name__ == '__main__':
     app.run(debug=True, port=7000)
